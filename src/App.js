98fe918d2b81df1061a9328f451ccdacd9af7719
soorajas1210 <soorajasofficial@gmail.com>
2023-01-09 10:12:49 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import React, { useState } from 'react'
+import './App.css';
+import Header from './components/Header';
+import Form from './components/Form'
+import Todolist from './components/Todolist';
 
-function App() {
+const App = () => {
+  
+  const [input, setInput] = useState("")
+  const [todos, setTodos] = useState([])
+  const [editTodo, setEditTodo] = useState(null);
+
+
   return (
-    <h1> sooraj world</h1>
+    <div className='container'>
+      <div className="app-wrapper">
+        <div>
+          <Header/>
+        </div>
+        <div>
+          <Form
+            input={input}
+          setInput ={setInput}
+          todos = {todos}
+          setTodos = {setTodos}
+          editTodo={editTodo}
+          setEditTodo = {setEditTodo}
+          ></Form>
+      </div>
+      <div>
+        <Todolist todos= {todos} setTodos = {setTodos} setEditTodo={setEditTodo}></Todolist>
+      </div>
+    </div>
+    </div >
   );
 }
 
